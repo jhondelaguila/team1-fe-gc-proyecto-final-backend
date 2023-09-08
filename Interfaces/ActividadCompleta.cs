8599fe869277c20a25e6279dd9f0f5ed7c3982c7
AlbertEsponey4 <126797937+AlbertEsponey4@users.noreply.github.com>
2023-09-08 03:28:35 +0200

    Feat(actividades): CRUD completado a falta de controlar errores

---
+++ Interfaces/ActividadCompleta.cs
@@ -0,0 +1,19 @@
+﻿using team1_fe_gc_proyecto_final_backend.Models;
+
+namespace team1_fe_gc_proyecto_final_backend.Interfaces
+{
+    public class ActividadCompleta
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int IdDireccion { get; set; }
+        public string Pais { get; set; }
+        public string Calle { get; set; }
+        public int Numero { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Provincia { get; set; }
+        public string Localidad { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+    }
+}
