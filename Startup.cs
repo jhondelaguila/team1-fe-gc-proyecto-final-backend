9d759de22b2474a4dfa0ef21524827a0dd1383f5
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-09-18 23:13:20 +0200

    allowOrigin Cors

--- Startup.cs
+++ Startup.cs
@@ -58,6 +58,7 @@ namespace team1_fe_gc_proyecto_final_backend
             app.UseCors(options =>
             {
                 options.WithOrigins("https://develop.d2i81o118zpx0s.amplifyapp.com", "https://localhost:4200");
+                options.AllowAnyOrigin();
                 options.AllowAnyMethod();
                 options.AllowAnyHeader();
             });
